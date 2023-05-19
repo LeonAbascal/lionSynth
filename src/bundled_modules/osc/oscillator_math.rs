310bd4d55133cc37b41c9c4b355d9b0f2ae27745
León Abascal <72978970+XARP616@users.noreply.github.com>
2023-05-19 23:22:11 +0200

    Test and bug fixing

--- src/bundled_modules/osc/oscillator_math.rs
+++ src/bundled_modules/osc/oscillator_math.rs
@@ -49,8 +49,11 @@ impl OscillatorMath for f32 {
         linear(value, y, m)
     }
 
+    /// Π_
+    /// Pulse 1 [0, π)
+    /// Rest -1 [π, 2π)
     fn sqr(&self) -> Self {
-        if *self % (2.0 * PI) > PI {
+        if *self % (2.0 * PI) < PI {
             1.0
         } else {
             -1.0
@@ -58,12 +61,14 @@ impl OscillatorMath for f32 {
     }
 
     /// pwd ranges from 0 to 2π
+    /// pulse 1 [0, pulse_width)
+    /// rest -1 [pulse_width, 2π)
     fn pulse(&self, pwd: f32) -> Self {
         let pwd = pwd % (2.0 * PI);
-        if *self % (2.0 * PI) > pwd {
+        if *self % (2.0 * PI) < pwd {
             1.0
         } else {
-            0.0
+            -1.0
         }
     }
 }
@@ -75,10 +80,70 @@ mod test {
     #[test]
     fn test_sqr() {
         let test_value_low = 0.0f32;
-        let test_value_mid = 0.0f32;
-        let test_value_top = 0.0f32;
+        let test_value_mid_a = PI - 0.001;
+        let test_value_mid_b = PI;
+        let test_value_top_a = 2.0 * PI - 0.001;
+        let test_value_top_b = 2.0 * PI;
 
-        assert_eq!(test_value_low.sqr(), 0.0);
-        assert_eq!(test_value_mid.sqr(), 0.0);
+        assert_eq!(test_value_low.sqr(), 1.0);
+        assert_eq!(test_value_mid_a.sqr(), 1.0);
+        assert_eq!(test_value_mid_b.sqr(), -1.0);
+        assert_eq!(test_value_top_a.sqr(), -1.0);
+        assert_eq!(test_value_top_b.sqr(), 1.0);
+    }
+
+    #[test]
+    fn test_pulse() {
+        let test_value_low = 0.0f32;
+        let test_value_mid = PI;
+        let test_value_top = 2.0 * PI;
+
+        let mut pulse_width = 0.0;
+        assert_eq!(test_value_low.pulse(pulse_width), -1.0);
+        assert_eq!(test_value_mid.pulse(pulse_width), -1.0);
+        assert_eq!(test_value_top.pulse(pulse_width), -1.0);
+
+        let mut pulse_width = PI;
+        assert_eq!(test_value_low.pulse(pulse_width), 1.0);
+        assert_eq!((PI - 0.001).pulse(pulse_width), 1.0);
+        assert_eq!(test_value_mid.pulse(pulse_width), -1.0);
+        assert_eq!((PI * 2.0 - 0.001).pulse(pulse_width), -1.0);
+        assert_eq!(test_value_top.pulse(pulse_width), 1.0);
+
+        let mut pulse_width = 2.0 * PI - 0.001;
+        assert_eq!(test_value_low.pulse(pulse_width), 1.0);
+        assert_eq!(test_value_mid.pulse(pulse_width), 1.0);
+        assert_eq!(test_value_top.pulse(pulse_width), 1.0);
+
+        let mut pulse_width = 2.0 * PI;
+        assert_eq!(test_value_low.pulse(pulse_width), -1.0);
+        assert_eq!(test_value_mid.pulse(pulse_width), -1.0);
+        assert_eq!(test_value_top.pulse(pulse_width), -1.0);
+    }
+
+    #[test]
+    fn test_saw() {
+        let test_value_low = 0.0f32;
+        let test_value_mid = PI;
+        let test_value_top_a = 2.0 * PI - 0.001;
+        let test_value_top_b = 2.0 * PI;
+
+        assert_eq!(test_value_low.saw(), 1.0);
+        assert_eq!(test_value_mid.saw(), 0.0);
+        assert_eq!(test_value_top_a.saw(), -0.9996817);
+        assert_eq!(test_value_top_b.saw(), 1.0);
+    }
+
+    #[test]
+    fn test_tri() {
+        let test_value_low = 0.0f32;
+        let test_value_mid = PI;
+        let test_value_top_a = 2.0 * PI - 0.001;
+        let test_value_top_b = 2.0 * PI;
+
+        assert_eq!(test_value_low.tri(), -1.0);
+        assert_eq!(test_value_mid.tri(), 1.0);
+        assert_eq!(test_value_top_a.tri(), -0.9993634);
+        assert_eq!(test_value_top_b.tri(), -1.0);
     }
 }
